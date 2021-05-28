67ca3e4d456d59e861dfbb8a7cca228f55127dc0
LeviKaza <M.Jason.45@hotmail.fr>
2021-05-28 14:23:32 +0000

    add deconnexion

--- template/nav.php
+++ template/nav.php
@@ -16,8 +16,18 @@ session_start();
                 <ul>
                     <li><a href="index.php">Acceuil</a></li>
                     <li><a href="users.php">Utilisateurs</a></li>
-                    <li><a href="form.php">Inscription</a></li>
-                    <li><a href="connexion.php">Connexion</a></li>
+                    <li><a href="form.php">Inscription</a></li> 
+                    <?php
+                    if (isset($_SESSION['name'])) {
+                        echo "<li>";
+                        echo "<p>Bonjour " . $_SESSION['name'] . "</p>";
+                        echo "<a href='controllers/deconnexion.php'>Deconnexion</a>";
+                        echo "</li>";
+                    }
+                    else {
+                        echo "<li><a href='connexion.php'>Connexion</a></li>";
+                    }
+                    ?>
                 </ul>
             </nav>
         </header>
